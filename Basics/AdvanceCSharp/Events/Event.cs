1fab2ad42a8351b4bfeb76076912db6c2c7aa5a9
HaranathRaju <haranathraju09@gmail.com>
2025-12-03 14:06:45 +0530

    added events concept

---
+++ Basics/AdvanceCSharp/Events/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics.AdvanceCSharp.Events
+{
+
+    public delegate void Notify();
+
+    public class process 
+    {
+        public event Notify On;
+
+        public void Start()
+        {
+            Console.WriteLine("process is completed");
+        }
+
+    }
+    internal class Event
+    {
+        public static void Main(string[] args)
+        {
+            process p=new process();
+            p.On += Handler;
+            p.Start();
+
+        }
+        public static void Handler()
+        {
+            Console.WriteLine("Process Completed Event Received!");
+        }
+    }
+}
